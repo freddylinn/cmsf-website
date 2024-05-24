cb259efab314b7ac91f4465ac2745b4e165a8f26
Freddy <frederick.linn@colorado.edu>
2024-05-24 13:33:07 -0600

    add print styles

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,20 +1,20 @@
 
 function Cell({checked, color, notes}) {
 
-    let styles = "w-36 p-2 bg-default border border-slate-700";
+    let styles = "w-36 p-2 bg-default print:bg-default border border-slate-700";
     let checkedStyles = "";
     switch(color){
         case -1:
-            checkedStyles = "w-36 p-2 bg-red-300 border border-slate-700";
+            checkedStyles = "w-36 p-2 bg-red-300 print:bg-red-300 border border-slate-700";
         break;
         case 0:
-            checkedStyles = "w-36 p-2 bg-slate-100 border border-slate-700";
+            checkedStyles = "w-36 p-2 bg-slate-100 print:bg-slate-100 border border-slate-700";
         break;
         case 1:
-            checkedStyles = "w-36 p-2 bg-yellow-200 border border-slate-700";
+            checkedStyles = "w-36 p-2 bg-yellow-200 print:bg-yellow-200 border border-slate-700";
         break;
         case 2:
-            checkedStyles = "w-36 p-2 bg-green-300 border border-slate-700";
+            checkedStyles = "w-36 p-2 bg-green-300 print:bg-green-300 border border-slate-700";
         break;
         default:
             checkedStyles = styles;
