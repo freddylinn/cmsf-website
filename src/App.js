d50202dc39ca79807dd094746f104c1b783d50f0
Freddy <frederick.linn@colorado.edu>
2024-05-28 15:12:44 -0600

    start count functionality

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import charData from './data/characteristics.json';
 import locData from './data/locations.json';
 
 import Row from './components/Row';
+import { useState } from 'react';
 
 function App() {
 
   const characteristics = charData;
   const locations = locData;
-  const firstRow = Object.keys(locations).map(item => <th colSpan={locations[item].length} className="p-4 border border-slate-700">{item}</th>)
-  const secondRow = Object.values(locations).flatMap(arr => arr).map(value => <th className="p-4 border border-slate-700">{value}</th>);
-  const charRows = Object.keys(characteristics).map(groupName => Object.entries(characteristics[groupName]).map(item => <Row rowData={item}/>))
+
+  const initialCount = Object.values(locations).flatMap(arr => arr).map(value => 0);
+  const [counts, setCounts] = useState({
+    "Yellow": initialCount,
+    "Green": initialCount,
+    "Red": initialCount, 
+    "Total": initialCount
+  });
+  const yellowCells = counts["Yellow"].map((item, index) => <td key={index} className="p-2 border border-slate-700">{item}</td>)
+  const greenCells = counts["Green"].map((item, index) => <td key={index} className="p-2 border border-slate-700">{item}</td>)
+  const redCells = counts["Red"].map((item, index) => <td key={index} className="p-2 border border-slate-700">{item}</td>)
+  const totalCells = counts["Total"].map((item, index) => <td key={index} className="p-2 border border-slate-700">{item}</td>)
+
+  const firstRow = Object.keys(locations).map(item => <th colSpan={locations[item].length} key={item} className="p-4 border border-slate-700">{item}</th>);
+  const secondRow = Object.values(locations).flatMap(arr => arr).map(value => <th key={value} className="p-4 border border-slate-700">{value}</th>);
+  const charRows = Object.keys(characteristics).map(groupName => Object.entries(characteristics[groupName]).map(item => <Row key={item} rowData={item} setCounts={setCounts}/>));
+
 
   return (
     <div className="">
@@ -28,6 +43,31 @@ function App() {
         <tbody>
           {charRows}
         </tbody>
+      </table>
+      <h2 className="mt-8 text-center">*Count functionality still in progress</h2>
+      <table className="table-auto text-center border border-slate-700 border-collapse mx-auto mt-1">
+        <thead>
+          <tr>
+            <th className="p-4 border border-slate-700"></th>
+            {secondRow}
+          </tr>
+          <tr>
+            <td className="bg-yellow-200 p-2 border border-slate-700 font-semibold">Total Yellow Count</td>
+            {yellowCells}
+          </tr>
+          <tr>
+            <td className="bg-green-300 p-2 border border-slate-700 font-semibold">Total Green Count</td>
+            {greenCells}
+          </tr>
+          <tr>
+            <td className="bg-red-300 p-2 border border-slate-700 font-semibold">Total Red Count</td>
+            {redCells}
+          </tr>
+          <tr>
+            <td className="w-48 p-2 border border-slate-700 font-semibold">Total Yellow & Green Count Subtracting Red</td>
+            {totalCells}
+          </tr>
+        </thead>
       </table>
     </div>
 
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,18 +1,29 @@
 import Cell from "./Cell";
 import { useState } from "react";
 
-function Row({rowData}){
+function Row({rowData, setCounts}){
 
     const [isChecked, setIsChecked] = useState(false);
 
     const title = rowData[0];
     const cellValues = rowData[1];
-    const cells = cellValues.map(item => <Cell checked={isChecked} color={item[0]} notes={item[1]}/>)
+    const cells = cellValues.map((item, index) => <Cell key={index} checked={isChecked} color={item[0]} notes={item[1]}/>)
 
     const titleDefault = "bg-slate-100 w-48 py-4 px-1 border border-slate-700"
     const titleChecked = "bg-white w-48 py-4 px-1 border border-slate-700"
     const checkboxDefault = "bg-slate-100 p-4 px-1 border border-slate-700"
     const checkboxChecked = "bg-white p-4 px-1 border border-slate-700"
+
+   /* const toggleCheck = () => {
+        for(val in cellValues){
+            if(val[0] == -1){
+                setCounts((prevCounts) => ({
+                    ...prevCounts,
+                    "Red": prevCounts["Red"][] + 1,
+                }));
+            }
+        }
+    } */
 
 
     return(
