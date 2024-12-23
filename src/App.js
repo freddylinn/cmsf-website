9fc105952682b4ca6937cfa8f86c6c3cb0422677
Freddy <frederick.linn@colorado.edu>
2024-12-23 13:40:10 -0500

    router fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Tool from "./pages/Tool";
 import Intro from "./pages/Intro";
 import Layout from "./components/Layout";
