af4f7d0552a9046eb2bc3b6ae9fb12da737d7a39
al5ilice <65573254+al5ilice@users.noreply.github.com>
2026-03-31 14:09:02 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // Added this
 import "./index.css";
 import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Wrapping App in HashRouter allows all your navigation links to work */}
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
